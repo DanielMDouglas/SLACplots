aa47d5a9f48fdacd8038105fc463cd433fa37035
Daniel Douglas <dougl215@slac.stanford.edu>
2025-03-27 21:45:32 -0700

    updates to colormap registration, some additional configs

--- SLACplots/colors.py
+++ SLACplots/colors.py
@@ -30,19 +30,17 @@ white = [256./256, 256./256, 256./256]
 SLACpaloverde = [39./256, 153./256, 137./256]
 SLACredRGB = [140./256, 21./256, 21./256]
 
-matplotlib.cm.register_cmap('SLACverde',
-                            ListedColormap(np.array([np.interp(np.linspace(0, 1, 256),
-                                                               [0, 1],
-                                                               [whiteV, pvV])
-                                                     for whiteV, pvV in zip(white, SLACpaloverde)]).T,
-                                           name = 'SLACverde'))
+matplotlib.colormaps.register(ListedColormap(np.array([np.interp(np.linspace(0, 1, 256),
+                                                                 [0, 1],
+                                                                 [whiteV, pvV])
+                                                       for whiteV, pvV in zip(white, SLACpaloverde)]).T,
+                                             name = 'SLACverde'))
 
-matplotlib.cm.register_cmap('SLACred',
-                            ListedColormap(np.array([np.interp(np.linspace(0, 1, 256),
-                                                               [0, 1],
-                                                               [whiteV, srV])
-                                                     for whiteV, srV in zip(white, SLACredRGB)]).T,
-                                           name = 'SLACred'))
+matplotlib.colormaps.register(ListedColormap(np.array([np.interp(np.linspace(0, 1, 256),
+                                                                 [0, 1],
+                                                                 [whiteV, srV])
+                                                       for whiteV, srV in zip(white, SLACredRGB)]).T,
+                                             name = 'SLACred'))
 
 LaTeXflavor = {"numu": r'$\nu_\mu$',
                "numubar": r'$\bar{\nu}_\mu$',
@@ -53,12 +51,16 @@ LaTeXflavor = {"numu": r'$\nu_\mu$',
 
 matplotlib.rc('axes', **{"prop_cycle": matplotlib.cycler(color = SLACcolors)})
 matplotlib.rc('image', **{"cmap": 'SLACverde'})
-# matplotlib.rc('font', **{"family": 'sans-serif',
-#                          "sans-serif": 'Arial',
-#                          "size": 16,
-#                          "weight": 'bold'})
-# matplotlib.rc('font', **{"family": 'sans-serif',
-#                          "sans-serif": 'Arial',
-#                          "size": 16,
-#                          "weight": 'bold'})
-# matplotlib.rc('text', **{"usetex": True})
+matplotlib.rc('font', **{"family": 'sans-serif',
+                         "sans-serif": 'Arial',
+                         "size": 16,
+                         "weight": 'bold'})
+matplotlib.rc('font', **{"family": 'sans-serif',
+                         "sans-serif": 'Arial',
+                         "size": 16,
+                         "weight": 'bold'})
+matplotlib.rc('text', **{"usetex": True})
+matplotlib.rc('lines', **{"linewidth": 3})
+matplotlib.rc('hist', **{"bins": 26})
+matplotlib.rc('savefig', **{"dpi": 300})
+matplotlib.rc('legend', **{"fancybox": True})
