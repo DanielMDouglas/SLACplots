766dba179f81a1e26b1c530259dedb0da5bd3e7d
Daniel Douglas <dougl215@slac.stanford.edu>
2025-05-21 00:37:18 -0700

    don't import rcparams by default

--- SLACplots/rcparams.py
+++ SLACplots/rcparams.py
@@ -8,7 +8,7 @@ matplotlib.rc('font', **{"family": 'sans-serif',
                          "sans-serif": 'Arial',
                          "size": 16,
                          "weight": 'bold'})
-matplotlib.rc('text', **{"usetex": False})
+matplotlib.rc('text', **{"usetex": True})
 matplotlib.rc('lines', **{"linewidth": 3})
 matplotlib.rc('hist', **{"bins": 26})
 matplotlib.rc('savefig', **{"dpi": 300})
